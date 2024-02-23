acb03f6961d908dc718737df0c8e1b39bc1c12fa
chan-hyeok-kim <daiolous@gmail.com>
2024-02-23 22:07:28 +0900

    RayCasting

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -32,6 +32,17 @@ public class TestCollision : MonoBehaviour
 
     void Update()
     {
-        
+                 //월드 좌표(플레이어가 바라보는 방향을 월드좌표로)
+        Vector3 look = transform.TransformDirection(Vector3.forward);
+
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, look, 10);
+
+        foreach(RaycastHit hit in hits)
+        {
+            Debug.Log($"RayCast! {hit.collider.gameObject.name} !");
+        }
     }
 }
